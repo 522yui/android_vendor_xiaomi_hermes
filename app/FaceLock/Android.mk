c8743bdd883df9db82bd1ab033bd9864c6bab932
Lok Jian Ming <jianminglok@hotmail.com>
2016-05-12 14:20:10 +0800

    Update Android.mk

--- app/FaceLock/Android.mk
+++ app/FaceLock/Android.mk
@@ -8,6 +8,6 @@ LOCAL_MODULE_TAGS   := optional
 LOCAL_MODULE_CLASS  := APPS
 LOCAL_CERTIFICATE   := platform
 LOCAL_MODULE_SUFFIX := .apk
-LOCAL_SRC_FILES     := app/EngineerMode/FaceLock.apk
+LOCAL_SRC_FILES     := app/FaceLock/FaceLock.apk
 LOCAL_MODULE_PATH   := $(PRODUCT_OUT)/system/app
 include $(BUILD_PREBUILT)
